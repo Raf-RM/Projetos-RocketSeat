e5e2bd4c88d64f30ffa870941eb972c30f9c4314
Raf-RM <rafael.r.mateus@gmail.com>
2023-05-05 13:35:42 -0300

    Inicio Desafio 1 Stage 6 - html, pages, js

---
+++ Codigo-Desafio-01-SPA-Stage-06/js/index.js
@@ -0,0 +1,33 @@
+function route(event) {
+  event = event || window.event
+  event.preventDefault()
+
+  window.history.pushState({},"",event.target.href)
+
+  handle()
+}
+
+const routes = {
+  "/":"/pages/home.html",
+  "/universe":"/pages/universe.html",
+  "/exploration":"/pages/exploration.html",
+}
+
+
+
+function handle() {
+  const {pathname} = window.location
+  const route = routes[pathname]
+
+  fetch(route)
+  .then(data => data.text())
+  .then(html => {
+    document.querySelector("#app").innerHTML = html
+  })
+}
+
+handle()
+
+window.onpopstate = () => handle()
+window.route = () => route()
+
