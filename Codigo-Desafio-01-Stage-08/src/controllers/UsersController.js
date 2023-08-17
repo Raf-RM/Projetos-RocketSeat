515267f54118a5a3c7d0c4879a2b40c39f871e7e
Raf-RM <rafael.r.mateus@gmail.com>
2023-08-17 10:48:30 -0300

    add pasta utils tratamento de excecoes

--- Codigo-Desafio-01-Stage-08/src/controllers/UsersController.js
+++ Codigo-Desafio-01-Stage-08/src/controllers/UsersController.js
@@ -1,6 +1,14 @@
+const AppError = require("../utils/AppError")
+
+
 class UserController {
   create(request, response) {
     const { name, email, password } = request.body;
+
+    if(!name){
+      throw new AppError("É obrigatório informar um nome.")
+    }
+
     response.send({ name, email, password });
   } 
 }
