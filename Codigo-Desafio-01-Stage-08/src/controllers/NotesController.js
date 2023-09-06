1fa4c09a138be064b4a0096bf60c4d214b94f045
Raf-RM <rafael.r.mateus@gmail.com>
2023-09-06 15:15:56 -0300

    add verificacao do rating fornecido pelo usuario

--- Codigo-Desafio-01-Stage-08/src/controllers/NotesController.js
+++ Codigo-Desafio-01-Stage-08/src/controllers/NotesController.js
@@ -1,9 +1,15 @@
 const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
 
 class NotesController {
   async create(request, response) {
     const { title, description, rating, tags } = request.body;
     const { user_id } = request.params;
+
+    if(rating < 1 || rating > 5) {
+      throw new AppError("O rating deve variar entre 1 e 5")
+
+    }
 
     const [note_id] = await knex("movie_notes").insert({
       title, 
