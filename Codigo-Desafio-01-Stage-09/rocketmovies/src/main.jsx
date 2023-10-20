70d5a77bc611c76b64ddf6f713031729b525b53c
Raf-RM <rafael.r.mateus@gmail.com>
2023-10-20 09:20:35 -0300

    add page SignIn finalizada

--- Codigo-Desafio-01-Stage-09/rocketmovies/src/main.jsx
+++ Codigo-Desafio-01-Stage-09/rocketmovies/src/main.jsx
@@ -4,14 +4,14 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/global';
 import theme from './styles/theme';
 
-import { MoviePreview } from './pages/MoviePreview';
+import { SignIn } from './pages/SignIn';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle/>
-      <MoviePreview/>
+      <SignIn/>
     </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ Codigo-Desafio-01-Stage-09/rocketmovies/src/pages/SignIn/index.jsx
@@ -0,0 +1,35 @@
+import { Container, Form, Background } from "./styles";
+
+import { FiMail, FiLock } from "react-icons/fi";
+
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
+import { ButtonText } from '../../components/ButtonText';
+
+export function SignIn(){
+  return(
+    <Container>
+      <Form>
+        <header>
+          <h1>RocketMovies</h1>
+          <p>Aplicação para acompanhar tudo que assistir.</p>
+        </header>
+
+        <h2>Faça seu Login</h2>
+
+        <div>
+          <Input placeholder='E-mail' type='text' icon={FiMail} />
+          <Input placeholder='Senha' type='password' icon={FiLock} />
+        </div>
+
+        <Button label='Entrar' />
+
+        <ButtonText label='Criar conta'  rote='/' />
+
+      </Form>
+
+      <Background/>
+
+    </Container>
+  )
+}
