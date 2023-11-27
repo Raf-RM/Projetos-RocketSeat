c22788c5b0c17998770d6caf38053d91cef466e8
Raf-RM <rafael.r.mateus@gmail.com>
2023-11-27 08:17:12 -0300

    add funcionalidade verificar password e email

--- Codigo-Desafio-01-Stage-10/backend/src/controllers/SessionsController.js
+++ Codigo-Desafio-01-Stage-10/backend/src/controllers/SessionsController.js
@@ -1,9 +1,23 @@
-
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+const { compare } = require("bcryptjs");
 
 class SessionsController {
   async create(request, response){
     const { email, password } = request.body;
-    return response.json({email, password});
+    const user = await knex("users").where({email}).first();
+    
+    if(!user){
+      throw new AppError("E-mail e/ou senha incorretos", 401)
+    }
+    
+    const correctPassword = await compare(password, user.password);
+    
+    if(!correctPassword){
+      throw new AppError("E-mail e/ou senha incorretos", 401)
+    }
+
+    return response.json(user);
   }
 };
 
