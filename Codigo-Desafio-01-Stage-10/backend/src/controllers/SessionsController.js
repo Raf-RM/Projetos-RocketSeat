8afc1194ae10a8fca8f8d2e3ce8f7dfcdfe66399
Raf-RM <rafael.r.mateus@gmail.com>
2023-11-27 08:29:38 -0300

    funcionalidade gerar token de usuario

--- Codigo-Desafio-01-Stage-10/backend/src/controllers/SessionsController.js
+++ Codigo-Desafio-01-Stage-10/backend/src/controllers/SessionsController.js
@@ -1,6 +1,8 @@
 const knex = require("../database/knex");
 const AppError = require("../utils/AppError");
 const { compare } = require("bcryptjs");
+const authConfig = require("../configs/auth");
+const { sign } = require("jsonwebtoken");
 
 class SessionsController {
   async create(request, response){
@@ -17,7 +19,12 @@ class SessionsController {
       throw new AppError("E-mail e/ou senha incorretos", 401)
     }
 
-    return response.json(user);
+    const { secret, expiresIn } = authConfig.jwt;
+    const token = sign({}, secret, {
+      subect:String(user.id),
+      expiresIn});
+
+    return response.json({user, token});
   }
 };
 
