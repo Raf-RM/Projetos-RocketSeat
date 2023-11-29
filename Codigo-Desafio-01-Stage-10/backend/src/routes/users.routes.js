2baa42da5c61e25fede8f2a5bbe292a17e83c7b2
Raf-RM <rafael.r.mateus@gmail.com>
2023-11-29 16:53:50 -0300

    atualizando rota de upload do avatar

--- Codigo-Desafio-01-Stage-10/backend/src/routes/users.routes.js
+++ Codigo-Desafio-01-Stage-10/backend/src/routes/users.routes.js
@@ -5,15 +5,17 @@ const uploadConfig = require("../configs/upload");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const UsersController = require("../controllers/UsersController");
+const UserAvatarController = require("../controllers/UserAvatarController");
 
 const usersRoutes = Router();
 
 const upload = multer(uploadConfig.MULTER);
 
 const usersController = new UsersController();
+const usersAvatarController = new UserAvatarController();
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAuthenticated, usersController.update);
-usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), usersController.update);
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), usersAvatarController.update);
 
 module.exports = usersRoutes;
